c8052ac60406b816546ab6062dc290f4c5a494c4
Trinh Hien <trinhthihien05091995@gmail.com>
2016-08-24 08:11:08 +0700

    PR[#73560] Repository

---
+++ app/Repositories/BaseRepositoryInterface.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repositories;
+
+interface BaseRepositoryInterface
+{
+    public function count();
+
+    public function all();
+
+    public function find($id);
+
+    public function findBy($attribute, $value, $columns = ['*']);
+
+    public function paginate($limit);
+
+    public function create($inputs);
+
+    public function update($inputs, $id);
+
+    public function delete($ids);
+
+    public function lists($column, $key = null);
+
+    public function where($column, $value = null, $operator = null);
+
+    public function whereIn($column, $values);
+
+    public function orWhere($column, $operator = null, $value = null);
+
+    public function orderBy($column, $direction = 'asc');
+
+    public function save(array $data, $exist_field = 'id')
+
+}
